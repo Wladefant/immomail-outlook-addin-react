88d840179bda8c1ad999102980813486fa0e6e6c
Wladimir Kirianov <wladefant@gmail.com>
2024-08-26 13:46:21 +0200

    * Remove unused `navigateToFrame` prop from `Frame3` component

--- src/taskpane/index.tsx
+++ src/taskpane/index.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/App";
 import { FluentProvider, webLightTheme } from "@fluentui/react-components";
+import Frame1 from "./components/Frame1";
+import Frame2 from "./components/Frame2";
+import Frame3 from "./components/Frame3";
 
 /* global document, Office, module, require, HTMLElement */
 
@@ -10,13 +12,25 @@ const title = "Contoso Task Pane Add-in";
 const rootElement: HTMLElement | null = document.getElementById("container");
 const root = rootElement ? createRoot(rootElement) : undefined;
 
-/* Render application after Office initializes */
-Office.onReady(() => {
-  root?.render(
+const App: React.FC = () => {
+  const [currentFrame, setCurrentFrame] = React.useState("Frame1");
+
+  const navigateToFrame = (frame: string) => {
+    setCurrentFrame(frame);
+  };
+
+  return (
     <FluentProvider theme={webLightTheme}>
-      <App title={title} />
+      {currentFrame === "Frame1" && <Frame1 navigateToFrame={navigateToFrame} />}
+      {currentFrame === "Frame2" && <Frame2 navigateToFrame={navigateToFrame} />}
+      {currentFrame === "Frame3" && <Frame3 navigateToFrame={navigateToFrame} />}
     </FluentProvider>
   );
+};
+
+/* Render application after Office initializes */
+Office.onReady(() => {
+  root?.render(<App />);
 });
 
 if ((module as any).hot) {
